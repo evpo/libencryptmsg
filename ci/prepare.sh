64ddf676aaf9db3be4285ac844bad636bce15ffe
Evgeny Pokhilko <software@evpo.net>
2018-03-06 21:08:51 +1100

    Fix the config command to use system directory

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -3,7 +3,5 @@ set -exu
 
 git clone https://github.com/randombit/botan $HOME/botan
 pushd $HOME/botan
-./configure.py --prefix="$HOME/stage/" --with-debug-info --cxxflags="-fno-omit-frame-pointer"
-#${MAKE} -j${CORES} install
-make install
+./configure.py --enable-modules=aes,pbkdf2,zlib --link-method=symlink
 popd
