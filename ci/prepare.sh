6cdcabb939382c059b803e74044ee0a462623b08
Evgeny Pokhilko <software@evpo.net>
2018-03-06 20:49:28 +1100

    Build botan in travis

---
+++ ci/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -exu
+
+git clone https://github.com/randombit/botan $HOME/botan
+pushd $HOME/botan
+./configure.py --prefix="$HOME/stage/" --with-debug-info --cxxflags="-fno-omit-frame-pointer"
+#${MAKE} -j${CORES} install
+${MAKE} install
+popd
