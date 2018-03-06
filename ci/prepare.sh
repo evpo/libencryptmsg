1e056f0cfbdf57b114946818c73fc583c92444c8
Evgeny Pokhilko <software@evpo.net>
2018-03-06 20:57:10 +1100

    fix the make statement

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -5,5 +5,5 @@ git clone https://github.com/randombit/botan $HOME/botan
 pushd $HOME/botan
 ./configure.py --prefix="$HOME/stage/" --with-debug-info --cxxflags="-fno-omit-frame-pointer"
 #${MAKE} -j${CORES} install
-${MAKE} install
+make install
 popd
