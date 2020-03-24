9896cd47f46b06621e1bea5aad8db5e52c7d0e13
Evgeny Pokhilko <software@evpo.net>
2020-03-24 14:09:15 +1100

    Follow the same approach in mingw for the amalgamation library as in OSX

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -7,9 +7,9 @@ git clone https://github.com/randombit/botan $HOME/botan
 ( cd $HOME/botan && git checkout 2.7.0 )
 pushd $HOME/botan
     if [[ "$env_name" == "appveyor" ]]; then
-        ./configure.py --prefix=./out --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --amalgamation --disable-shared --cc gcc --cpu x86 --os mingw
+        ./configure.py --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --amalgamation --disable-shared --cc gcc --cpu x86 --os mingw
     elif [[ "$env_name" == "osx" ]]; then
-        ./configure.py --prefix=./out --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --link-method=symlink --amalgamation --disable-shared
+        ./configure.py --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --link-method=symlink --amalgamation --disable-shared
     elif [[ "$env_name" == "linux" ]]; then
         ./configure.py --prefix=/usr --enable-modules=aes,pbkdf2,zlib --link-method=symlink
     fi
