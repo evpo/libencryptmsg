f20f486d1a3cce5ffe50588e08008a71e8587fa5
Evgeny Pokhilko <software@evpo.net>
2020-03-24 12:58:25 +1100

    Select tag 2.7.0 in Botan

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -4,6 +4,7 @@ set -exu
 env_name=$1
 
 git clone https://github.com/randombit/botan $HOME/botan
+( cd $HOME/botan && git checkout 2.7.0 )
 pushd $HOME/botan
     if [[ "$env_name" == "appveyor" ]]; then
         ./configure.py --prefix=./out --enable-modules=aes,pbkdf2,auto_rng,compression,zlib --amalgamation --disable-shared --cc gcc --cpu x86 --os mingw
